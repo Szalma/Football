881c3db79603171d390d9d02cd80d4fe147e1fe9
Szalma <stefania.szalma@gmail.com>
2021-06-30 12:31:12 +0200

    Football

---
+++ footballmatch/src/main/java/football/Beer.java
@@ -0,0 +1,21 @@
+package football;
+
+public class Beer {
+    private String brand;
+    private int quantity;
+
+    public String getBrand () {
+        return brand;
+    }
+
+
+    public int getQuantity () {
+        return quantity;
+    }
+
+
+    public Beer(Integer quantity, String brand){
+        this.quantity=quantity;
+        this.brand=brand;
+    }
+}
